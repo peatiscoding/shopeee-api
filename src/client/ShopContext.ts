036012a3187c7506c6e266347ed9804db5cd10d2
Bee <thammawat@muze.co.th>
2022-11-09 14:17:00 +0700

    fix import shopee order detail response type

--- src/client/ShopContext.ts
+++ src/client/ShopContext.ts
@@ -16,6 +16,7 @@ import {
   ShopeeProductModelListResponse,
   ShopeeOrderStatus,
   ShopeeGetOrderListResponse,
+  ShopeeOrdersDetailResponse,
 } from '../models'
 import {
   createShopeeAutoRefreshHandler,
