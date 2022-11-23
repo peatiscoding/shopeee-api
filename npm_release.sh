907610796d16bb8486c9d748bea4a636247ff7a9
Kittiphat S <freeuxer@gmail.com>
2022-11-23 13:50:49 +0700

    update npm release script

--- npm_release.sh
+++ npm_release.sh
@@ -34,13 +34,13 @@ CODE_VERSION=$(cat $CODE_PATH/package.json | grep -m 1 version | sed 's/[^0-9.]/
 echo "(NODE)\n\tRequired by nvm '$NVM_REQUIRED';\n\tRequired by package.json '$PACKAGE_JSON_REQUIRED';\n\tCurrent '$NODE_VERSION';"
 echo "(PACKAGE)\n\tON REGISTRY '$NPM_VERSION';\n\tSource Code '$CODE_VERSION';"
 
-if [[ "$NODE_VERSION" == $PACKAGE_JSON_REQUIRED ]]
-then
-  echo "NODE Version satisfied package.json.";
-else
-  echo "NODE Version did not satisfied package json requirement $PACKAGE_JSON_REQUIRED. but got $NODE_VERSION."
-  exit 1;
-fi
+# if [[ "$NODE_VERSION" == $PACKAGE_JSON_REQUIRED ]]
+# then
+#   echo "NODE Version satisfied package.json.";
+# else
+#   echo "NODE Version did not satisfied package json requirement $PACKAGE_JSON_REQUIRED. but got $NODE_VERSION."
+#   exit 1;
+# fi
 
 if [[ "$NODE_VERSION" == $NVM_REQUIRED ]]
 then
@@ -58,5 +58,4 @@ fi
 
 npm run build
 npm publish
-git tag release-v$CODE_VERSION
-# git push
+git push
